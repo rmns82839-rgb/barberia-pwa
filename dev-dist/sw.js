017d8a3c9fbb5319c0cfc9cd9bdbb5ffd1dff83a
rmns82839-rgb <tu-email@gmail.com>
2026-07-23 18:23:27 -0500

    fix: iconos PWA (icon-192, icon-512, favicon.ico) y referencia en index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ovcd4bnsilo"
+    "revision": "0.b4n5aad91n8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
